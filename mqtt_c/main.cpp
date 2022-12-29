4ef67667cd5abe30f6cc49c1ae6e0d1c607bff89
chieubkhn <chieubkhn@gmail.com>
2022-12-29 23:18:20 +0700

    init mqtt_c project

---
+++ mqtt_c/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+	int a = 10;
+	a++;
+	std::cout<< "a="<< a;
+	return 0;
+
+}
---
+++ mqtt_c/mqtt_pub.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <mosquitto.h>
+
+int main()
+{
+
+	int rc;
+	struct mosquitto* mosq;
+	mosquitto_lib_init();
+	mosq = mosquitto_new("publisher-test", true, NULL);
+	rc = mosquitto_connect(mosq, "localhost", 1883, 60);
+	if(rc != 0){
+
+	printf("client can not connect to broker");
+	mosquitto_destroy(mosq);
+	return -1;
+	}
+	printf("connect success to broker");
+	mosquitto_publish(mosq, NULL, "test/t1", 6, "hello", 0, false);
+	mosquitto_disconnect(mosq);
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+
+	return 0;
+
+
+
+}
